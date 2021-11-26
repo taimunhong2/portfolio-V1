d88c604026d78b62ec01b1d65028031f5c5e4221
Tai Mun Hong <72969130+taimunhong2@users.noreply.github.com>
2021-11-26 19:45:51 +0800

    finish desktop view

---
+++ app.js
@@ -0,0 +1,29 @@
+
+function sendEmail() {
+    console.log("going to send")
+  var tempParams = {
+    from_name: document.getElementById("name").value,
+    to_name: "Mun Hong",
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  }
+  
+  emailjs.send('service_uzft0dr', 'template_9jy2yd5', tempParams).then(function (res) {
+    console.log("success", res.status)
+    if (res.status != 200) {
+      alert("Fail to send!")
+    }
+    else {
+      alert("message sent!")
+    }
+    }
+    )
+}
+
+
+
+
+
+
+
